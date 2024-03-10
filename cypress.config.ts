ccef1cc5833364890c289eb34a9280702083d0cf
Yevand <99153027+Yevand@users.noreply.github.com>
2024-03-10 18:05:37 +0100

    cypress.config.ts has been modified

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.{spec,test}.{js,jsx}',
-    viewportWidth: 1920,
-    viewportHeight: 1080,
+    viewportWidth: 1440,
+    viewportHeight: 900,
     defaultCommandTimeout: 4000,
     retries: { runMode: 1, openMode: 1 },
     scrollBehavior: 'center',
